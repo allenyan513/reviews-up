7acaa6bbf78ded2b415ced37aa0a1e12034df409
alin <wsyanligang@gmail.com>
2025-06-10 20:04:28 -0300

    1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  const port = process.env.API_PORT;
+  if (!port) {
+    throw new Error('Port is required');
+  }
+  await app.listen(port);
 }
+
 bootstrap();
