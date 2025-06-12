e1531f63ba5ca1f5a990b5bb0604da223130decd
alin <wsyanligang@gmail.com>
2025-06-12 15:04:49 -0300

    fix dont send email magic link, if the user is not exist

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -65,6 +65,7 @@ export class AuthController {
     return res.redirect(`${process.env.APP_URL}`);
   }
 
+  @UseGuards(EmailMagicGuard)
   @Post('send-magic-link')
   async sendLink(@Body('email') email: string) {
     return this.authService.sendMagicLink(email);
