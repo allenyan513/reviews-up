84038e307b38b118fec4ce32c6291f71de91b82b
alin <wsyanligang@gmail.com>
2025-06-12 21:03:23 -0300

    ci/ci rebuild

--- apps/web/app/[lang]/(auth)/layout.tsx
+++ apps/web/app/[lang]/(auth)/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { PropsWithChildren } from 'react';
 
-import '@/app/globals.css';
 import { UserProvider } from '@/context/UserProvider';
 import ToastContext from '@/context/ToastContext';
 
