70bef95b2dee60821ccce5078942e245524b4334
alin <wsyanligang@gmail.com>
2025-06-15 19:45:31 -0300

    .env

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,12 +1,34 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+import dotenv from 'dotenv';
+import * as path from 'path';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
+
+// Get the current file's directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ['localhost'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "s3.us-east-2.amazonaws.com",
-      }
+        protocol: 'https',
+        hostname: 's3.us-east-2.amazonaws.com',
+      },
     ],
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID:
+    process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WWW_URL: process.env.NEXT_PUBLIC_WWW_URL,
+    NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+    NEXT_PUBLIC_AWS_S3_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+  },
 };
+
+export default nextConfig;
