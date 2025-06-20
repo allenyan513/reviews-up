703b48ed9bc62b5985e8d2eb79a596a029742014
alin <wsyanligang@gmail.com>
2025-06-19 23:41:11 -0300

    twitter oauth2.0

--- apps/api/src/modules/auth/strategies/twitter.strategy.ts
+++ apps/api/src/modules/auth/strategies/twitter.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-twitter';
+import { Strategy } from '@superfaceai/passport-twitter-oauth2';
 import { AuthService } from '@src/modules/auth/auth.service';
 
 @Injectable()
@@ -9,15 +9,24 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
 
   constructor(private authService: AuthService) {
     super({
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      clientType:'confidential',
+      clientID: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
       callbackURL: process.env.TWITTER_CALLBACK_URL,
     });
   }
 
-  async validate(...args: any[]): Promise<any> {
-    this.logger.debug('Arguments received in TwitterStrategy validate:', args);
-    const { accessToken, refreshToken, profile }: any = args;
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: (err: any, user?: any) => void,
+  ): Promise<any> {
+    this.logger.debug('Arguments received in TwitterStrategy validate:', {
+      accessToken,
+      refreshToken,
+      profile,
+    })
     const {
       provider,
       id,
