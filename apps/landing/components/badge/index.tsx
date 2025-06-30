39d81e74625db570315b3724f3b9bf94c9ddba69
alin <wsyanligang@gmail.com>
2025-06-30 14:14:09 -0300

    add starupfa badge

--- apps/landing/components/badge/index.tsx
+++ apps/landing/components/badge/index.tsx
@@ -1,10 +1,12 @@
 import {Turbo0} from "@/components/badge/turbo0";
+import {Startupfa} from "@/components/badge/startupfa";
 
 
 export function Badge(){
   return (
     <div className="flex flex-row gap-2">
       <Turbo0/>
+      <Startupfa/>
     </div>
   )
 }
--- apps/landing/components/badge/turbo0.tsx
+++ apps/landing/components/badge/turbo0.tsx
@@ -3,7 +3,7 @@ export function Turbo0() {
     <a href="https://turbo0.com/item/reviewsupio" target="_blank" rel="noopener noreferrer">
       <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0"
            style={{
-             height: '54px',
+             height: '44px',
              width: 'auto',
            }}
       />
