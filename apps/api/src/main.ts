05ae83d87ec4b74b976357767ff9d283f876f60a
alin <wsyanligang@gmail.com>
2025-07-03 12:12:36 -0300

    1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.NEXT_PUBLIC_APP_URL, process.env.NEXT_PUBLIC_WWW_URL],
-    credentials: true,
+    origin: '*',
   });
   await app.listen(5500);
 }
