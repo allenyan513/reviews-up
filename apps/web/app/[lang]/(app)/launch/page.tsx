227aad08942156def1ea323e03a6be65dbf015de
alin <wsyanligang@gmail.com>
2025-07-14 02:24:04 -0300

    fix /launch

--- apps/web/app/[lang]/(app)/launch/page.tsx
+++ apps/web/app/[lang]/(app)/launch/page.tsx
@@ -1,21 +1,19 @@
 'use client';
 import { redirect } from 'next/navigation';
-import { useSession } from '@/context/UserProvider';
-import { UserEntity } from '@reviewsup/api/users';
+import { useEffect } from 'react';
+import { api } from '@/lib/api-client';
 
 export default function Page(props: { params: Promise<{ lang: string }> }) {
-  const { user } = useSession({
-    required: true,
-    onUnauthenticated: (user: UserEntity) => {
-      redirect(
-        `/auth/signin?redirect=${encodeURIComponent(`/${user.Workspace[0].id}/promotion/my-products`)}`,
-      );
-    },
-  });
-
-  if (user) {
-    // user is authenticated, redirect to the promotion page
-    redirect(`/${user.Workspace[0].id}/promotion/my-products`);
-  }
+  useEffect(() => {
+    api.auth.getSession().then((user) => {
+      if (!user) {
+        return redirect(
+          `/auth/signin?redirect=${encodeURIComponent(`/${user.Workspace[0].id}/promotion/my-products`)}`,
+        );
+      } else {
+        return redirect(`/${user.Workspace[0].id}/promotion/my-products`);
+      }
+    });
+  }, []);
   return null;
 }
